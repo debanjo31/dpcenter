e48a89a44064507fdac3a11005e7c5693b73f5bc
Ayo <heyayomideadebisi@gmail.com>
2023-06-29 13:32:51 +0100

    chore: remove empty component folder

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -4,9 +4,9 @@ const Homepage = () => {
         <h1 className="text-3xl font-semibold">Homepage</h1>
         <p>Lets build refine hackaton my bro.</p>
 
-        <p>Note: I've removed every existing folder and file, and left the components and pages folder</p>
-        <p>the pages will house the different pages/routes, and the components will house the components of those pages/routes</p>
-        <p>so right now, we have an homepage and auth folder in both components and pages folder.</p>
+        <p>Note: I've removed every existing folder and file, and left the pages folder</p>
+        <p>the pages will house the different pages/routes</p>
+        <p>so right now, we have an homepage and auth folder in pages folder.</p>
     </div>
   )
 }
