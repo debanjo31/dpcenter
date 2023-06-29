aee8ee55cc9417f90cc4ee2884963e0937042d52
Ayo <heyayomideadebisi@gmail.com>
2023-06-29 13:20:25 +0100

    cleanup

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,14 @@
+const Homepage = () => {
+  return (
+    <div className="mt-20 text-center">
+        <h1 className="text-3xl font-semibold">Homepage</h1>
+        <p>Lets build refine hackaton my bro.</p>
+
+        <p>Note: I've removed every existing folder and file, and left the components and pages folder</p>
+        <p>the pages will house the different pages/routes, and the components will house the components of those pages/routes</p>
+        <p>so right now, we have an homepage and auth folder in both components and pages folder.</p>
+    </div>
+  )
+}
+
+export default Homepage
