7d801cf48d5a445e622831a1cba9176bc5a7fee4
debanjo31 <debanjo31@gmail.com>
2023-07-05 17:30:10 +0100

    supabase keys added

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://omxgfeuyewlmhorxfokh.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9teGdmZXV5ZXdsbWhvcnhmb2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgzNjMwODIsImV4cCI6MjAwMzkzOTA4Mn0.P-C7eeMf_WPOGHk_SmYj1tJfZKF3G3JwejArARA7t1c";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
